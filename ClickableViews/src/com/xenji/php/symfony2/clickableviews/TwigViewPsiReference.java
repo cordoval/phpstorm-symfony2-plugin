2c24183be1e8bc60467dfa2834c270e10e2830a9
mmueller <mario@xenji.com>
2012-06-05 06:40:43 +0200

    Extended docs + impl for the handle rename

--- ClickableViews/src/com/xenji/php/symfony2/clickableviews/TwigViewPsiReference.java
+++ ClickableViews/src/com/xenji/php/symfony2/clickableviews/TwigViewPsiReference.java
@@ -1,5 +1,6 @@
 package com.xenji.php.symfony2.clickableviews;
 
+import com.intellij.lang.ASTNode;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiDirectory;
@@ -10,34 +11,72 @@ import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.ProjectScope;
 import com.intellij.util.IncorrectOperationException;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import com.jetbrains.php.lang.psi.elements.impl.StringLiteralExpressionImpl;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Reference implementation for Twig Templates.
+ *
+ * @author Mario Mueller mario@xenji.com
+ * @license MIT
+ */
 public class TwigViewPsiReference implements PsiReference {
 
+    /**
+     * The template string from e.x. a controller
+     */
     private StringLiteralExpression templateString;
 
+    /**
+     * Project reference, which is needed e.x. for the
+     * FilenameIndex.
+     */
     private Project project;
 
+    /**
+     * the resolved file, which is to be cached in here.
+     */
     private PsiFile resolvedFile;
 
+    /**
+     * Lazy cache for a cleaned string - which means a string without
+     * quotations.
+     */
     private String cleanString;
 
+    /**
+     *
+     * @param templateString
+     * @param project
+     */
     public TwigViewPsiReference(final StringLiteralExpression templateString, Project project) {
         this.templateString = templateString;
         this.project = project;
         cleanString = templateString.getText().replace("\"", "").replace("'", "");
     }
 
+    /**
+     * @see com.intellij.psi.PsiReference#getElement()
+     * @return the full source element
+     */
     @Override
     public PsiElement getElement() {
         return templateString;
     }
 
+    /**
+     * @see com.intellij.psi.PsiReference#getRangeInElement()
+     * @return the full range incl. quotes.
+     */
     @Override
     public TextRange getRangeInElement() {
         return this.templateString.getTextRange();
     }
 
+    /**
+     * @see com.intellij.psi.PsiReference#resolve()
+     * @return the resolved template file or null
+     */
     @Override
     public PsiElement resolve() {
 
@@ -62,6 +101,7 @@ public class TwigViewPsiReference implements PsiReference {
         PsiFile bundleFile = filesByName[0];
         PsiDirectory bundleDir = bundleFile.getContainingDirectory();
 
+        //@TODO check for NPEs
         PsiDirectory resourcesDir = bundleDir.findSubdirectory("Resources");
         PsiDirectory viewsDir = resourcesDir.findSubdirectory("views");
         PsiDirectory ctlDir = viewsDir.findSubdirectory(ctrl);
@@ -69,14 +109,32 @@ public class TwigViewPsiReference implements PsiReference {
         return resolvedFile;
     }
 
+    /**
+     * @see com.intellij.psi.PsiReference#getCanonicalText()
+     * @return Plain text representation.
+     */
     @NotNull
     @Override
     public String getCanonicalText() {
         return this.templateString.getText();
     }
 
+    /**
+     * @see com.intellij.psi.PsiReference#handleElementRename(String)
+     * @param newElementName
+     * @return the new string literal with the new text
+     * @throws IncorrectOperationException
+     */
     @Override
     public PsiElement handleElementRename(String newElementName) throws IncorrectOperationException {
+        ASTNode node = templateString.getNode();
+        StringLiteralExpressionImpl se = new StringLiteralExpressionImpl(node);
+
+        String[] parts = templateString.getText().split(":");
+        StringBuilder sb = new StringBuilder(parts[0]).append(":").append(parts[1]).append(":");
+        sb.append(newElementName);
+        se.updateText(sb.toString());
+        this.templateString = se;
         return templateString;
     }
 
--- ClickableViews/src/com/xenji/php/symfony2/clickableviews/TwigViewPsiReferenceProvider.java
+++ ClickableViews/src/com/xenji/php/symfony2/clickableviews/TwigViewPsiReferenceProvider.java
@@ -7,11 +7,20 @@ import com.intellij.util.ProcessingContext;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Reference Provider implementation for Twig Templates.
+ *
+ * @author Mario Mueller mario@xenji.com
+ * @license MIT
+ */
 public class TwigViewPsiReferenceProvider extends PsiReferenceProvider {
     @NotNull
     @Override
     public PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+
+        // IF we touch down here, we do have a StringLiteralExpression.
         StringLiteralExpression se = (StringLiteralExpression) element;
+
         TwigViewPsiReference psiReference = new TwigViewPsiReference(se, element.getProject());
 
         if (psiReference.resolve() != null) {
@@ -19,5 +28,4 @@ public class TwigViewPsiReferenceProvider extends PsiReferenceProvider {
         }
         return PsiReference.EMPTY_ARRAY;
     }
-
 }
